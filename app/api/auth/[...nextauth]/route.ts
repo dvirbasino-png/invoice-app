70b1a3d6b6b328bc5ac3402c6c0836670210967b
dvirbasino-png <dvirbasino@vega.io>
2026-06-08 13:44:15 +0300

    fix build errors

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,11 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string
+  }
+}
 
 const handler = NextAuth({
   providers: [
